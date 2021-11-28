78f87caf151ff55e72be25f528837c2da5d9fc9e
Saqib <saqib@Saqibs-MacBook-Pro.local>
2021-11-28 23:28:32 +0100

    Initial commit

--- restaurant_system/models.py
+++ restaurant_system/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    restaurantName = models.CharField(max_length = 50)
+    restaurantAddress = models.CharField(max_length=100) 
