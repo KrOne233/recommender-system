964e2b0223ff885f662688efe589b337aa95185e
Saqib <saqib@Saqibs-MBP.fritz.box>
2022-01-22 00:41:50 +0100

    login and registration functionality

--- restaurant_system/models.py
+++ restaurant_system/models.py
@@ -4,3 +4,14 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     rating = models.FloatField()
+
+
+
+class RestaurantSystemUser(models.Model):
+    user = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    restaurant = models.TextField(blank=True, null=True)
+
+    #class Meta:
+     #   managed = False
+      #  db_table = 'restaurant_system_user'
--- restaurant_system/views.py
+++ restaurant_system/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from cgitb import html
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Restaurant
+from .models import RestaurantSystemUser
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # # Create your views here.
 # def welcome_message(request):
@@ -14,12 +22,57 @@ def welcome_message(request):
 def show_cover(request):
     return render(request, 'cover.html')
 
-def catalog(request):
-    data = {'restaurants': Restaurant.objects.all()}
+def get_all_ratings(request):
+    data = {'rating': RestaurantSystemUser.objects.all()}
     return render(request, 'catalog.html',data)
 
-def login_page(request):
-    return render(request, 'login.html')
+def get_all_restaurants(request):
+    restaurantName =  RestaurantSystemUser.objects.all()
+    html = ''
+    for r in restaurantName:
+        html += f'<h1>{r.reataurant}'
+    return render(request, 'catalog.html', restaurantName)
+    #return render(request, 'catalog.html',restaurantName)
+
+def get_restaurants(request):
+    data = {'reataurant':RestaurantSystemUser.objects.all()}
+    return render(request,'catalog.html',data)
+    
 
 def user_registration(request):
-    return render (request, 'registration.html')
+    if request.user.is_authenticated:
+        return redirect('catalog')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account successfully created for' + user)
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request, 'registration.html',context)
+
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('catalog')
+    else:
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect('catalog')
+            else: 
+                messages.info(request, 'Username/Password incorrect')
+
+        context = {}
+        return render (request, 'login.html' , context)
+
+def logoutuser(request):
+    logout(request)
+    return redirect ('login')
