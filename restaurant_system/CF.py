0ffc2a9cb742ae4c832f319ae2d2ffcaadcca239
KrOne <zwang2@uni-muenster.de>
2022-01-30 14:22:04 +0100

    recommender model

--- restaurant_system/CF.py
+++ restaurant_system/CF.py
@@ -6,6 +6,7 @@ from surprise import SVD
 from surprise import accuracy
 from surprise.model_selection import cross_validate
 from surprise import dump
+from fuzzywuzzy import fuzz
 
 df = pd.read_excel("user_dataset_final.xlsx")
 df_algo = df.sample(frac=0.9)
@@ -22,3 +23,22 @@ pred = algo.predict(user_id, item_id, r_ui, verbose=True)
 dump.dump('recommender', algo, verbose=1)
 
 df.iloc[:, 2].drop_duplicates(keep='first').to_csv("restaurant.csv", index=False)
+
+# matching the restaurants of restaurant_rating and restaurant_CO2
+restaurant_CO2 = pd.read_csv("restaurant_CO2.csv")
+restaurant = pd.read_csv("restaurant.csv")
+CO2 = list()
+for r in restaurant.iloc[:, 0]:
+    i = 0
+    for c in restaurant_CO2.iloc[:, 1]:
+        if fuzz.ratio(r.lower(), c.lower()) >= 90:
+            CO2.append(float(restaurant_CO2.iloc[i, 2]))
+            i = -1
+            break
+        else:
+            i += 1
+    if i != -1:
+        CO2.append(0)
+
+restaurant["CO2"] = CO2
+restaurant.to_csv("restaurant_CO2_final.csv")
