416594d75e97d66d7988deed0bea8f50021b571b
Saqib <saqib@Saqibs-MBP.fritz.box>
2022-01-31 20:15:58 +0100

    initial commit

--- restaurant_system/urls.py
+++ restaurant_system/urls.py
@@ -2,8 +2,8 @@ from unicodedata import name
 from django.urls import path
 from.import views
 urlpatterns = [
-    path('homepage/', views.welcome_message),
-    path('cover/', views.show_cover), #show_cover = name of function, cover is url
+    # path('homepage/', views.welcome_message),
+    # path('cover/', views.show_cover), #show_cover = name of function, cover is url
     path('catalog/', views.get_restaurants, name ="catalog"),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logoutuser, name='logout'),
